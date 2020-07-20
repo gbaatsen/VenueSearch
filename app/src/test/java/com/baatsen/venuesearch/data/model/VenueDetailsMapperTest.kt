fb1db90f558bd2a849ce8dfcf7dd7aecedae22cf
Gilles Baatsen <gilles@baatsen.nl>
2020-07-20 08:30:05 +0200

    added tests

--- app/src/test/java/com/baatsen/venuesearch/data/model/VenueDetailsMapperTest.kt
+++ app/src/test/java/com/baatsen/venuesearch/data/model/VenueDetailsMapperTest.kt
@@ -9,6 +9,10 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class VenueDetailsMapperTest {
     private val venueDetailsMapper = VenueDetailsMapper()
+    private val photo1 = Photo("http://www.nu.nl/", "photo1.jpg")
+    private val photo2 = Photo("http://www.nu.nl/", "photo2.jpg")
+    private val photo3 = Photo("http://www.nu.nl/", "photo3.jpg")
+    private val photo4 = Photo("http://www.nu.nl/", "photo4.jpg")
 
     @Test
     fun `formatted phone number is returned when available`() {
@@ -68,6 +72,118 @@ class VenueDetailsMapperTest {
                 "Blastraat 12, Amsterdam",
                 "Best thing in town",
                 "0206370926", null, 4.5, null
+            )
+        )
+    }
+
+    @Test
+    fun `one photo in one group is returned properly`() {
+        val venueDetailsResponse = VenueDetailsResponse(
+            Response(
+                VenueDetailsJson(
+                    id = "13",
+                    name = "Best Something",
+                    location = VenueLocation(address = "Blastraat 12, Amsterdam"),
+                    contact = Contact(phone = "0206370926"),
+                    description = "Best thing in town",
+                    rating = 4.5,
+                    photos = Photos(listOf(Group(listOf(photo1))))
+                )
+            )
+        )
+
+        val result = venueDetailsMapper.transform(
+            venueDetailsResponse
+        )
+
+        assertEquals(
+            result, VenueDetails(
+                "13",
+                "Best Something",
+                "Blastraat 12, Amsterdam",
+                "Best thing in town",
+                "0206370926", null, 4.5, listOf("http://www.nu.nl/height300photo1.jpg")
+            )
+        )
+    }
+
+    @Test
+    fun `two photos in one group are returned properly`() {
+        val venueDetailsResponse = VenueDetailsResponse(
+            Response(
+                VenueDetailsJson(
+                    id = "13",
+                    name = "Best Something",
+                    location = VenueLocation(address = "Blastraat 12, Amsterdam"),
+                    contact = Contact(phone = "0206370926"),
+                    description = "Best thing in town",
+                    rating = 4.5,
+                    photos = Photos(listOf(Group(listOf(photo1, photo2))))
+                )
+            )
+        )
+
+        val result = venueDetailsMapper.transform(
+            venueDetailsResponse
+        )
+
+        assertEquals(
+            result, VenueDetails(
+                "13",
+                "Best Something",
+                "Blastraat 12, Amsterdam",
+                "Best thing in town",
+                "0206370926",
+                null,
+                4.5,
+                listOf(
+                    "http://www.nu.nl/height300photo1.jpg",
+                    "http://www.nu.nl/height300photo2.jpg"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `two photos in two groups are returned properly`() {
+        val venueDetailsResponse = VenueDetailsResponse(
+            Response(
+                VenueDetailsJson(
+                    id = "13",
+                    name = "Best Something",
+                    location = VenueLocation(address = "Blastraat 12, Amsterdam"),
+                    contact = Contact(phone = "0206370926"),
+                    description = "Best thing in town",
+                    rating = 4.5,
+                    photos = Photos(
+                        listOf(
+                            Group(listOf(photo1, photo2)),
+                            Group(listOf(photo3, photo4))
+                        )
+                    )
+                )
+            )
+        )
+
+        val result = venueDetailsMapper.transform(
+            venueDetailsResponse
+        )
+
+        assertEquals(
+            result, VenueDetails(
+                "13",
+                "Best Something",
+                "Blastraat 12, Amsterdam",
+                "Best thing in town",
+                "0206370926",
+                null,
+                4.5,
+                listOf(
+                    "http://www.nu.nl/height300photo1.jpg",
+                    "http://www.nu.nl/height300photo2.jpg",
+                    "http://www.nu.nl/height300photo3.jpg",
+                    "http://www.nu.nl/height300photo4.jpg"
+                )
             )
         )
     }
